983b997a379f632c6cc49127743ef1ccc5bf4738
nafiz-nascenia <nafiz@nascenia.com>
2015-12-14 12:29:37 +0600

    Added relationship between ttf and engineers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
+
+  #creating self joins between columns for ttf and engineers
+  has_many :subordinates, class_name: "User", foreign_key: :ttf_id
+  belongs_to :ttf, class_name: "User"
+
+  def self.names
+    all.map(&:name)
+  end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
